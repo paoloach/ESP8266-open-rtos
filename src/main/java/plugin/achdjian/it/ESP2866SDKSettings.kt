7059b5b83ca83adfced787b292da240386949caf
paoloach <paolo.achdjian@skillbill.it>
2018-06-07 08:21:28 +0200

    work in progress

--- src/main/java/plugin/achdjian/it/ESP2866SDKSettings.kt
+++ src/main/java/plugin/achdjian/it/ESP2866SDKSettings.kt
@@ -2,24 +2,33 @@ package plugin.achdjian.it
 
 import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.project.Project
 import com.intellij.ui.layout.panel
 import javax.swing.JComponent
 import javax.swing.JTextField
 
-class ESP2866SDKSettings : ProjectComponent, Configurable {
+class ESP2866SDKSettings(val project:Project) : ProjectComponent, Configurable {
     val rootPath = JTextField()
     override fun isModified(): Boolean {
         return true
     }
 
-    override fun getDisplayName() = "ESP2866/"
+    override fun getDisplayName() = "ESP2866"
+
 
     override fun apply() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun createComponent()= panel(){
-            row("Root path: ") {rootPath  }
-        }
+    override fun disposeComponent() {
+
+    }
+
+    override fun createComponent() = panel() {
+        row("Root path: ") { rootPath }
+    }
+
+    override fun reset() {
+
     }
 }
