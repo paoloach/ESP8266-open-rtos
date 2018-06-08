aac6e2479a26a85558254e46b55306ab25cd6936
pachdjia <paolo_achdjian_tb@bmc.com>
2018-06-08 17:38:42 +0200

    Working on wizard

--- src/main/kotlin/plugin/achdjian/it/wizard/Step1.kt
+++ src/main/kotlin/plugin/achdjian/it/wizard/Step1.kt
@@ -5,8 +5,8 @@ import com.intellij.ide.util.projectWizard.ModuleBuilder
 import com.intellij.ide.util.projectWizard.ModuleWizardStep
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.roots.ModifiableRootModel
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.layout.Row
 import com.intellij.ui.layout.panel
@@ -15,7 +15,6 @@ import plugin.achdjian.it.ESP8266SettingsState
 import java.awt.event.ItemEvent
 import javax.swing.JCheckBox
 import javax.swing.JComponent
-import javax.swing.JScrollPane
 
 fun Row.checkBox(text: String, actionListener: (event: ItemEvent) -> Unit) {
     val checkBox = JCheckBox(BundleBase.replaceMnemonicAmpersand(text))
@@ -29,19 +28,19 @@ class Step1(val context: WizardContext) : ModuleWizardStep() {
         private val LOG = Logger.getInstance(Step1::class.java)
     }
 
-    val extras = arrayListOf("ad7709x","ads111x","bearssl","bh1750","bme680","bmp180","bmp280","ccs811","cpp_support","crc_generic",
-            "dhcpserver","dht","ds1302","ds1307","ds18b20","ds3231","dsm","fatfs","fonts","fram",
-            "hd44780","hmc5883l","http-parser","http_client_ota","httpd","i2c","i2s_dma","ina3221","jsmn","l3gd20h",
-            "libesphttpd","lis3dh","lis3mdl","lsm303d","max7219","mbedtls","mcp4725","mdnsresponder","ms561101ba03","multipwm @ 44ecea5",
-            "onewire","paho_mqtt_c","pca9685","pcf8574","pcf8591","pwm","rboot-ota","sdio","sht3x","sntp",
-            "softuart","spiffs","ssd1306","stdin_uart_interrupt","timekeeping","tsl2561","tsl4531","ultrasonic","wificfg","ws2812",
+    val extras = arrayListOf("ad7709x", "ads111x", "bearssl", "bh1750", "bme680", "bmp180", "bmp280", "ccs811", "cpp_support", "crc_generic",
+            "dhcpserver", "dht", "ds1302", "ds1307", "ds18b20", "ds3231", "dsm", "fatfs", "fonts", "fram",
+            "hd44780", "hmc5883l", "http-parser", "http_client_ota", "httpd", "i2c", "i2s_dma", "ina3221", "jsmn", "l3gd20h",
+            "libesphttpd", "lis3dh", "lis3mdl", "lsm303d", "max7219", "mbedtls", "mcp4725", "mdnsresponder", "ms561101ba03", "multipwm @ 44ecea5",
+            "onewire", "paho_mqtt_c", "pca9685", "pcf8574", "pcf8591", "pwm", "rboot-ota", "sdio", "sht3x", "sntp",
+            "softuart", "spiffs", "ssd1306", "stdin_uart_interrupt", "timekeeping", "tsl2561", "tsl4531", "ultrasonic", "wificfg", "ws2812",
             "ws2812_i2s")
-    val extrasEnabled = HashMap<String,Boolean>()
+    val extrasEnabled = HashMap<String, Boolean>()
 
 
     override fun getComponent(): JComponent {
-        val extraPanel = panel(title="Extra") {
-            extras.forEach{name -> row(){checkBox(name,{extrasEnabled[name] = it.stateChange == ItemEvent.SELECTED})}}
+        val extraPanel = panel(title = "Extra") {
+            extras.forEach { name -> row() { checkBox(name, { extrasEnabled[name] = it.stateChange == ItemEvent.SELECTED }) } }
         }
 
         return JBScrollPane(extraPanel)
@@ -50,14 +49,22 @@ class Step1(val context: WizardContext) : ModuleWizardStep() {
     override fun updateDataModel() {
         val projectBuilder = context.projectBuilder
         if (projectBuilder is EPS2866ModuleBuilder) {
-            projectBuilder.addModuleConfigurationUpdater(ConfigurationUpdater)
+            projectBuilder.addModuleConfigurationUpdater(ConfigurationUpdater(extrasEnabled))
         }
         context.projectBuilder
         System.out.println("Update data model")
-        val state = project.getComponent(ESP8266SettingsState::class.java, ESP2866SDKSettings.DEFAULT) as ESP8266SettingsState
-        state.extras = HashMap(extrasEnabled)
+//        val state = project.getComponent(ESP8266SettingsState::class.java, ESP2866SDKSettings.DEFAULT) as ESP8266SettingsState
+//        state.extras = HashMap(extrasEnabled)
     }
-
-    private object ConfigurationUpdater : ModuleBuilder.ModuleConfigurationUpdater()
-
 }
+
+class ConfigurationUpdater(val extras: Map<String, Boolean>) : ModuleBuilder.ModuleConfigurationUpdater() {
+
+    override fun update(module: Module, rootModel: ModifiableRootModel) {
+        val state = module.project.getComponent(ESP8266SettingsState::class.java, ESP2866SDKSettings.DEFAULT) as ESP8266SettingsState
+        state.extras = HashMap(extras)
+
+        rootModel.inheritSdk()
+    }
+}
+
